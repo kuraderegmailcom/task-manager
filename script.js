303eab9e1b2da8682f3b4c95856d437549ffbc66
kurabachew dereje <kuradere21@gmail.com>
2025-06-05 16:43:14 -0300

    add dom selector to the input filed
    in the function if the text input is empty it will popup alert message

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
 let tasks = [];
 let taskId = 0;
+function addTask() {
+  const taskInput = document.getElementById("taskInput");
+  const taskTitle = taskInput.value.trim();
+
+  if (taskTitle === "") {
+    alert("Task cannot be empty!");
+    return;
+  }
+   }
